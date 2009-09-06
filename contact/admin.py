1d6799e4262b95fcbc02cf0b37c4ec5884f80080
Joshua Partogi <jpartogi@scrum8.com>
2009-09-06 23:36:31 +1000

    enhanced the display of message list in admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,4 +1,4 @@
-# $Id: admin.py 324bfbdd83ad 2009/09/06 13:23:11 jpartogi $
+# $Id: admin.py e8fd065c6a51 2009/09/06 13:36:31 jpartogi $
 
 from django.contrib import admin
 from django.core import urlresolvers
@@ -11,7 +11,7 @@ class SubjectAdmin(admin.ModelAdmin):
 
     def department_url(self, obj):
         change_url = urlresolvers.reverse('admin:contact_department_change', args=(obj.department.id,))
-        return "<a href='%s'>%s</a>" % (change_url, obj.department.name)
+        return '<a href="%s">%s</a>' % (change_url, obj.department.name)
     department_url.allow_tags = True
     department_url.short_description = _('department')
 
@@ -19,7 +19,12 @@ class DepartmentAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'phone')
 
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ('sender_name', 'sender_email','created')
+    list_display = ('subject', 'sender', 'created')
+
+    def sender(self, obj):
+        return '<a href="mailto:%s">%s</a>' % (obj.sender_email, obj.sender_name)
+    sender.allow_tags = True
+    sender.short_description = 'sender'
 
 admin.site.register(Subject, SubjectAdmin)
 admin.site.register(Department, DepartmentAdmin)
