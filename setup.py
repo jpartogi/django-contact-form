6a8c3269fa2344aed3ffabf35f4d4e3dff01b1c5
Joshua Partogi <joshua.java@gmail.com>
2009-10-19 19:58:43 +0000

    added distutils install

--- setup.py
+++ setup.py
@@ -1,2 +1,29 @@
 from distutils.core import setup
 
+app_name = 'contact_form'
+
+setup(
+    name='django-contact-form',
+    version='0.9.0',
+    packages=[app_name, '%s.templatetags' % app_name],
+    package_data = {app_name: ['templates/%s/*.html' % app_name]},
+    author = 'Joshua Partogi',
+    author_email = 'joshua.partogi@gmail.com',
+    url = 'http://github.com/scrum8/django-contact-form',
+    download_url = 'http://github.com/scrum8/django-contact-form/downloads',
+
+    license = "BSD License",
+    keywords = "django contact form",
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    platforms = ['any'],
+)
